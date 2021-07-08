4c3eb2cb69365d234f605c3ea1fde48653e50f2c
Yasha <ysahakyan1998@gmail.com>
2021-07-08 17:39:03 +0400

    add item

---
+++ tiko.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Button from '@material-ui/core/Button';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = () => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    inputWrapper: {
+        margin: 25,
+        alignItems: 'stretch',
+        display: 'flex'
+    },
+    flex1: {
+        flex: 1
+    }
+})
+
+
+class Items extends React.Component {
+
+
+    render() {
+        const {cmp: {value}, classes} = this.props
+        return (
+            <div className={classes.root}>
+                <div className={classes.flex1}>
+                    {value}
+                </div>
+                <div>
+                    <Button
+                        variant="outlined"
+                        color="primary"
+                        value={value}
+                        onClick={this.handleEdit}
+                    >
+                        Edit
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        color="primary"
+                        value={value}
+                        onClick={this.handleRemoveOpen}
+                    >
+                        Remove
+                    </Button>
+                </div>
+            </div>
+        )
+    }
+}
+
